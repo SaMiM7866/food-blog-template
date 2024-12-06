63fd8b0f5c9df258311ca32f77c0c41f5a77e1ab
SaMiM7866 <129958735+SaMiM7866@users.noreply.github.com>
2024-12-06 21:33:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function samim_open() {
+    document.getElementById("mySidebar").classList.remove("hidden");
+  }
+   
+  function samim_close() {
+    document.getElementById("mySidebar").classList.add("hidden");
+  }
